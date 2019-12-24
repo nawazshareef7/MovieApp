2fbc0dc5410c4193286df35231af6079f8499873
Nawaz Sharif <nawaz.s@dplit.com>
2019-12-24 09:32:59 +0500

    --Test--

--- App.js
+++ App.js
@@ -1,114 +1,119 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {Component} from 'react';
+import { Text, View,StyleSheet,Image  } from 'react-native';
+import { NavigationNativeContainer, createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Container, Header, Left, Body, Right, Button, Title } from 'native-base';
+//import Icon from "react-native-vector-icons/FontAwesome";
+import { Appbar, Icon } from 'react-native-paper';
+import { createStackNavigator } from 'react-navigation-stack';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
+// class CustomHeader extends React.Component{
+//   render() {
+//     let {title , isHome} = this.props
+//     return(
+     
+//        <Appbar.Header style={{justifyContent:"center", backgroundColor:"white"}}>
+//         {/* <Appbar.BackAction
+//           onPress={this._goBack}
+//         /> */}
+//         <Appbar.Content style={{alignItems:"center", }}
+//           title="Home" 
+          
+//         />
+//         <Appbar.Action icon="filter" size={30} color="#900"  style={{position:"absolute", right:0}}   onPress={this._handleMore} />
+//       </Appbar.Header>
+      
+//     );
+//   }
+// }
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  top: {
+   backgroundColor:'white',
+    alignItems: "center"
   },
 });
 
-export default App;
+class HomeScreen extends React.Component{
+  render(){
+    return (
+      <View style={{ flex: 1 }}>
+        <Appbar.Header style={{justifyContent:"center", backgroundColor:"white"}}>
+        {/* <Appbar.BackAction
+          onPress={this._goBack}
+        /> */}
+        <Appbar.Content style={{alignItems:"center", }}
+          title="Home" 
+          
+        />
+        <Appbar.Action icon="filter" size={30} color="#900"  style={{position:"absolute", right:0}}   onPress={this._handleMore} />
+      </Appbar.Header>
+        <View style={{flex:1,justifyContent: 'center', alignItems: 'center'}}>
+        
+      <Text>Home!</Text>
+        </View>
+        
+    </View>
+    )
+  }
+}
+class SearchScreen extends React.Component{
+  render(){
+    return (
+      <View style={{ flex: 1 }}>
+        <Appbar.Header style={{justifyContent:"center", backgroundColor:"white"}}>
+        {/* <Appbar.BackAction
+          onPress={this._goBack}
+        /> */}
+        <Appbar.Content style={{alignItems:"center", }}
+          title="search" 
+          
+        />
+        <Appbar.Action icon="rocket" size={30} color="#900"  style={{position:"absolute", right:0}}   onPress={this._handleMore} />
+      </Appbar.Header>
+        <View style={{flex:1,justifyContent: 'center', alignItems: 'center'}}>
+        <Button title='Click Me' light>
+          <Text>Go to search</Text>
+        </Button>
+      <Text>Search!</Text>
+        </View>
+        
+    </View>
+    )
+  }
+}
+class MoreScreen extends React.Component{
+  render(){
+    return (
+      <View style={{ flex: 1 }}>
+        <Appbar.Header style={{justifyContent:"center", backgroundColor:"white"}}>
+        {/* <Appbar.BackAction
+          onPress={this._goBack}
+        /> */}
+        <Appbar.Content style={{alignItems:"center", }}
+          title="More" 
+          
+        />
+        <Appbar.Action icon="rocket" size={30} color="#900"  style={{position:"absolute", right:0}}   onPress={this._handleMore} />
+      </Appbar.Header>
+        <View style={{flex:1,justifyContent: 'center', alignItems: 'center'}}>
+        <Button title='Click Me' onPress={ ()=>this.props.navigation.navigate('Search')}>
+          <Text>Go to search</Text>
+        </Button>
+      <Text>More!</Text>
+        </View>
+        
+    </View>
+    )
+  }
+}
+
+
+const tabNavigator = createBottomTabNavigator({
+  Home:HomeScreen, 
+  Search: SearchScreen,
+  More: MoreScreen
+});
+
+
+
+export default createAppContainer(tabNavigator);
